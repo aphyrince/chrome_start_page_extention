5e79f536e16549b31d802926b19ec48b2d449607
aphyrince <dkswjdgh01@naver.com>
2026-05-17 17:59:56 +0900

    배경화면 애니메이션 기능 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,13 @@ import Clock from "../components/Clock";
 
 const App = () => {
     return (
-        <div className="flex flex-col justify-center w-full h-dvh bg-linear-60 from-dominant to-accent">
+        <div
+            className="flex flex-col justify-center w-full h-dvh animate-[shift-ratio_8s_ease-in-out_infinite_alternate]"
+            style={{
+                background:
+                    "linear-gradient(var(--bg-deg), var(--color-dominant) 0%, var(--color-surface) var(--bg-ratio), var(--color-accent) 100%)",
+            }}
+        >
             <Clock />
         </div>
     );
