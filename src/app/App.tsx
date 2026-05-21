9229da06545bcb2b35bdc5d4fcf47e2ed2380df9
aphyrince <dkswjdgh01@naver.com>
2026-05-21 18:48:45 +0900

    배경화면 애니메이션에서 deg 요소 삭제

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
-import Clock from "../components/Clock";
+import Clock from '../components/Clock';
 
 const App = () => {
     return (
         <div
-            className="flex flex-col justify-center w-full h-dvh animate-[shift-ratio_8s_ease-in-out_infinite_alternate]"
+            className='flex flex-col justify-center w-full h-dvh animate-[shift-ratio_8s_ease-in-out_infinite_alternate]'
             style={{
                 background:
-                    "linear-gradient(var(--bg-deg), var(--color-dominant) 0%, var(--color-surface) var(--bg-ratio), var(--color-accent) 100%)",
+                    'linear-gradient(60deg, var(--color-dominant) 0%, var(--color-surface) var(--bg-ratio), var(--color-accent) 100%)',
             }}
         >
             <Clock />
