5e30fbf7283ec0799547f9e8e12ff4f11bb1201f
aphyrince <dkswjdgh01@naver.com>
2026-05-22 13:35:57 +0900

    북마크스토어 훅 만듬.

--- src/hooks/useBookmarkStore.ts
+++ src/hooks/useBookmarkStore.ts
@@ -9,11 +9,23 @@ interface Bookmark {
 interface BookmarkStore {
     list: Bookmark[];
     add: (title: string, url: string) => void;
+    remove: (id: string) => void;
 }
 
 const useBookmarkStore = create<BookmarkStore>()((set) => ({
     list: [],
-    add: (title: string, url) => {},
+    add: (title: string, url: string) => {
+        const newBookmark = {
+            id: new Date().toDateString() + '_bookmark',
+            title,
+            url,
+        };
+        set((state) => ({ list: [...state.list, newBookmark] }));
+    },
+    remove: (id: string) => {
+        set((state) => ({ list: [...state.list.filter((item) => item.id !== id)] }));
+    },
 }));
 
+export default useBookmarkStore;
 export type { Bookmark };
