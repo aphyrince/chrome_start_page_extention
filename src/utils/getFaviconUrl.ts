90bcdc7c3836229734ea0e4ee39fb732299da480
aphyrince <dkswjdgh01@naver.com>
2026-05-25 16:20:41 +0900

    바로가기 추가에서 https 등을 안붙여도 작동하도록 getFaviconUrl.ts 함수 변경

--- src/utils/getFaviconUrl.ts
+++ src/utils/getFaviconUrl.ts
@@ -1,10 +1,17 @@
 function getFaviconUrl(pageUrl: string) {
+    let formattedUrl = pageUrl.trim();
+
+    // 1. http:// 나 https:// 로 시작하지 않으면 자동으로 https:// 를 붙여줌
+    if (!/^https?:\/\//i.test(formattedUrl)) {
+        formattedUrl = `https://${formattedUrl}`;
+    }
+
     // 크롬 내부의 _favicon 주소 생성
     // @ts-expect-error 무시 ㄱ
     const url = new URL(chrome.runtime.getURL('/_favicon/'));
 
     // 필수 파라미터 세팅 (대상 사이트 URL, 원하는 아이콘 크기)
-    url.searchParams.set('pageUrl', pageUrl);
+    url.searchParams.set('pageUrl', formattedUrl);
     url.searchParams.set('size', '32'); // 16, 32, 48, 64 등 지정 가능
 
     return url.toString();
