0ee5bc2dcaf3446758154fcdbcd167d3bb526aa5
aphyrince <dkswjdgh01@naver.com>
2026-05-26 20:00:57 +0900

    바로가기 누르면 해당 사이트가 열리도록 구현.

--- src/components/BookMark/BookMark.tsx
+++ src/components/BookMark/BookMark.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import useBookmarkStore from '../../hooks/useBookmarkStore';
 import getFaviconUrl from '../../utils/getFaviconUrl';
 
 const BookMark = () => {
     const bookmarkList = useBookmarkStore((s) => s.list);
 
+    const handleBookmarkClick = useCallback((url: string) => {
+        window.open(url, '_blank');
+    }, []);
+
     return (
         <div className='grid grid-cols-3 gap-2 text-white'>
             {bookmarkList.map((item) => (
                 <div key={item.id} className='flex flex-col justify-center items-center'>
-                    <button className='box-content p-2 rounded-md bg-white/30'>
+                    <button
+                        className='box-content p-2 rounded-md bg-white/30 hover:scale-90 duration-200 cursor-pointer'
+                        onClick={() => handleBookmarkClick(item.url)}
+                    >
                         <img src={getFaviconUrl(item.url)} alt={item.url}></img>
                     </button>
                     <p>{item.title}</p>
